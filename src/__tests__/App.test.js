3da711b5c68c2e43b2156f9c95df4fc6614eb842
kristiannilsson <kristian.nilsson@ymail.com>
2023-09-01 21:42:18 +0200

    tested for deletion  and mocked response from API

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,20 @@
 import { render, screen, waitFor, within } from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 import userEvent from "@testing-library/user-event";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import mockDrinks from "./mockDrinks.json";
+
+const server = setupServer(
+  rest.get(
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php",
+    (_req, res, ctx) => res(ctx.json({ drinks: mockDrinks }))
+  )
+);
+
+beforeAll(() => server.listen());
+
+afterAll(() => server.close());
 
 test("should render the correct header", () => {
   render(<App />);
@@ -39,6 +53,23 @@ test("should display new drink after submission via enter", async () => {
   expect(within(list).getByText("Alice Cocktail")).toBeInTheDocument();
 });
 
-test("should be able to delete a drink", () => {
-  
-})
+test("should be able to delete a drink", async () => {
+  render(<App />);
+  const user = userEvent.setup();
+
+  await waitFor(() =>
+    expect(screen.queryByText("Loading..")).not.toBeInTheDocument()
+  );
+
+  const listItems = screen.getAllByRole("listitem");
+  const listLength = listItems.length;
+  const listItem = listItems[0];
+  const removeBtn = within(listItem).getByAltText("remove button");
+
+  expect(listItem).toBeInTheDocument();
+  expect(removeBtn).toBeInTheDocument();
+
+  await user.click(removeBtn);
+  expect(listItem).not.toBeInTheDocument();
+  expect(listLength - screen.getAllByRole("listitem").length).toBe(1);
+});
