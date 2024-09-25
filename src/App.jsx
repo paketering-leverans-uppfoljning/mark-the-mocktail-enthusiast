220c071f89d218b9b53ac6c4ee4b50615da0f5b1
Kristian Nilsson <77685746+kristiannilsson@users.noreply.github.com>
2024-09-25 13:23:13 +0200

    added another subtitle

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
     <div className={styles["container"]}>
       <h1 className={styles["header"]}>Mark&apos;s To-drink List</h1>
       <h2>My favorite mocktails</h2>
+      <h2>Another subtitle</h2>
       <Searchbar mocktails={mocktails} dispatch={todoListDispatch} />
       <Filter filter={activeFilter} updateFilter={setActiveFilter} />
       <TodoList
